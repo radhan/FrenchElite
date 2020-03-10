6c907b53a2438fbab354c6d7a7d24c9acc501b04
ajouanne <alexandre.jouanne@capgemini.com>
2020-03-10 13:57:37 +0100

    facebook auth

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../shared/authentication-service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
